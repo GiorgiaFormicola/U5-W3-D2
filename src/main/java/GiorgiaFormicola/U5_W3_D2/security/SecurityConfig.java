81275a3e2c680e183dd37552725035853c10c3f5
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-21 14:50:59 +0200

    Adds CorsConfigurationSource Bean to SecurityConfig

--- src/main/java/GiorgiaFormicola/U5_W3_D2/security/SecurityConfig.java
+++ src/main/java/GiorgiaFormicola/U5_W3_D2/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package GiorgiaFormicola.U5_W3_D2.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -24,5 +30,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder getBCryptEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(@Value("${localhost.port}") String localhostPort) {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:" + localhostPort));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
