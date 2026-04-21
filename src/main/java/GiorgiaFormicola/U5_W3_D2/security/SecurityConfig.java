10c19ebe5da6c66f37df8e6d9e20625f879c8097
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-21 14:51:54 +0200

    Updates SecurityFilterChain for CORS

--- src/main/java/GiorgiaFormicola/U5_W3_D2/security/SecurityConfig.java
+++ src/main/java/GiorgiaFormicola/U5_W3_D2/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package GiorgiaFormicola.U5_W3_D2.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,6 +25,7 @@ public class SecurityConfig {
         httpSecurity.formLogin(formLogin -> formLogin.disable());
         httpSecurity.csrf((csrf -> csrf.disable()));
         httpSecurity.authorizeHttpRequests((request -> request.requestMatchers("/**").permitAll()));
+        httpSecurity.cors(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
