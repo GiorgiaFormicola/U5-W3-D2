7c685a74c9c2133bbf7ff8edd622cb61be34468c
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-21 14:37:29 +0200

    Adds password encoder in checkCredentialsAndGenerateToken() method

--- src/main/java/GiorgiaFormicola/U5_W3_D2/services/AuthService.java
+++ src/main/java/GiorgiaFormicola/U5_W3_D2/services/AuthService.java
@@ -6,18 +6,20 @@ import GiorgiaFormicola.U5_W3_D2.exceptions.UnauthorizedException;
 import GiorgiaFormicola.U5_W3_D2.payloads.LoginDTO;
 import GiorgiaFormicola.U5_W3_D2.security.TokenTools;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AuthService {
-    private EmployeesService employeesService;
-    private TokenTools tokenTools;
+    private final EmployeesService employeesService;
+    private final TokenTools tokenTools;
+    private final PasswordEncoder bCryptEncoder;
 
     public String checkCredentialsAndGenerateToken(LoginDTO body) {
         try {
             Employee found = this.employeesService.findByEmail(body.email());
-            if (!found.getPassword().equals(body.password()))
+            if (!bCryptEncoder.matches(body.password(), found.getPassword()))
                 throw new UnauthorizedException("Wrong credentials supplied");
             return this.tokenTools.generateToken(found);
         } catch (NotFoundException ex) {
